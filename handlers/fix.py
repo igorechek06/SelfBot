44c3908fca1fbfbce7f86d1c1d20f370d9d1e9cc
Igor Tolmachov <me@igorek.dev>
2022-04-06 09:47:11 +0900

    Bug fix

--- handlers/fix.py
+++ handlers/fix.py
@@ -7,6 +7,10 @@ from client import app
 @app.on_message(me & forwarded)
 async def forward(_, msg: Message) -> None:
     m = await app.get_messages(msg.chat.id, msg.message_id - 1)
-    if m.forward_date is None and m.from_user.id == msg.from_user.id:
+    if (
+        m.forward_date is None
+        and m.from_user.id == msg.from_user.id
+        and m.date + 1 >= msg.date
+    ):
         await m.copy(msg.chat.id, reply_to_message_id=msg.message_id)
         await m.delete()

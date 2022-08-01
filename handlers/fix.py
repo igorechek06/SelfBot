6864fe0d0cca231d8ef7cb543e3a5a732bd682be
Igor Tolmachov <me@igorek.dev>
2022-08-01 22:48:07 +0900

    Add '!save' and '!steal' commands

--- handlers/fix.py
+++ handlers/fix.py
@@ -1,7 +1,9 @@
-from pyrogram.filters import forwarded, me
+from pyrogram.filters import command, forwarded, me, reply
 from pyrogram.types import Message
 
 from client import app
+from libs.errors import throw
+from libs.util import copy
 
 
 @app.on_message(me & forwarded)
@@ -18,3 +20,30 @@ async def forward(_, msg: Message) -> None:
     ):
         await m.copy(msg.chat.id, reply_to_message_id=msg.message_id)
         await m.delete()
+
+
+@app.on_message(me & command("steal", "!"))
+async def steal(_, msg: Message) -> None:
+    args = msg.command[1:]
+    await throw("Too few arguments ({message_link})", msg, len(args) >= 1)
+    try:
+        link = args[0].split("/")
+        c_id, m_id = link[-2:]
+        if link[3] == "c":
+            m_id = int(m_id)
+            c_id = int(f"-100{c_id}")
+        else:
+            m_id = int(m_id)
+            c_id = str(c_id)
+    except Exception:
+        await throw("Incorrect arguments", msg)
+
+    await msg.delete()
+    m = await app.get_messages(c_id, m_id)
+    await copy(m, msg.chat.id, msg.reply_to_message_id)
+
+
+@app.on_message(me & command("save", "!") & reply)
+async def save(_, msg: Message) -> None:
+    await msg.delete()
+    await copy(msg.reply_to_message, "self")
